bd9b3ba6bd5247204e19b11dcbf424b249a66f8d
Ryan Sasik <sasryat@dunwoody.edu>
2018-01-25 09:27:40 -0600

    <corrected arrays to perform correctly>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,44 +16,42 @@ namespace nfl_draft_project
 			string selectionString = Console.ReadLine();
 			int selectionInt = Int32.Parse(selectionString);
 			return selectionInt;
-
-
 		}
 
 		static void Main(string[] args)
 		{
-			string[] players =
+			string[,] players =
 			{
-			"Mason Rudolph", "Lamar Jackson", "Josh Rosen", "Sam Darnold", "Baker Mayfield",
-			 "Saquon Barkley","Derrius Guice", "Bryce Love","Ronald Jones 11","Damien Harris",
-			 "Courtland Sutton","James Washington","Marcell Ateman","Anthony Miller","Calvin Ridley",
-			 "Maurice Hurst","Vita Vea","Taven Bryan","Da'Ron Payne","Harrison Phillips",
-			 "Joshua Jackson","Derwin James","Denzal Ward","Minkah Fitzpatrick","Isiah Oliver",
-			 "Mark Andrews","Dallas Goedert","Jaylen Samuels","Mike Gesicki","Troy Fumagalli",
-			 "Roquan Smith","Tremaine Edmundo","Kendall Joseph","Dorian O'Daniel","Malik Jefferson",
-			 "Orlando Brown","Kolton Miller","Chukwuma Okorafor","Connor Williams","Mike McGlinchey"
+			{ "Mason Rudolph", "Lamar Jackson", "Josh Rosen", "Sam Darnold", "Baker Mayfield" },
+			{ "Saquon Barkley","Derrius Guice", "Bryce Love","Ronald Jones 11","Damien Harris" },
+			 { "Courtland Sutton","James Washington","Marcell Ateman","Anthony Miller","Calvin Ridley" },
+			{ "Maurice Hurst","Vita Vea","Taven Bryan","Da'Ron Payne","Harrison Phillips" },
+			{ "Joshua Jackson","Derwin James","Denzal Ward","Minkah Fitzpatrick","Isiah Oliver" },
+			 { "Mark Andrews","Dallas Goedert","Jaylen Samuels","Mike Gesicki","Troy Fumagalli" },
+			 { "Roquan Smith","Tremaine Edmundo","Kendall Joseph","Dorian O'Daniel","Malik Jefferson" },
+			{ "Orlando Brown","Kolton Miller","Chukwuma Okorafor","Connor Williams","Mike McGlinchey"}
 			};
-			string[] school =
+			string[,] school =
 			{
-			"Oklahoma State","Louisville","UCLA","Southern California","Oklahoma",
-			"Penn State", "LSU","Stanford","Southern California","Alabama",
-			"Southern Methodist","Oklahoma State","Oklahoma State","Memphis","Alabama",
-			"Michigan","Washington","Florida","Alabama","Stanford",
-			"Iowa","Florida State","Ohio State","Alabama","Colorado",
-			"Oklahoma","So. Dakota State","NC State","Penn State","Wisconsin",
-			"Georgia","Virgina Tech","Clemson","CLemson","Texas",
-			"Oklahoma","UCLA","Western Michigan","Texas","Notre Dame"
+			{ "Oklahoma State","Louisville","UCLA","Southern California","Oklahoma" },
+			{ "Penn State", "LSU","Stanford","Southern California","Alabama" },
+			{ "Southern Methodist","Oklahoma State","Oklahoma State","Memphis","Alabama" },
+			{ "Michigan","Washington","Florida","Alabama","Stanford" },
+			{ "Iowa","Florida State","Ohio State","Alabama","Colorado" },
+			{ "Oklahoma","So. Dakota State","NC State","Penn State","Wisconsin" },
+			{ "Georgia","Virgina Tech","Clemson","CLemson","Texas" },
+			{ "Oklahoma","UCLA","Western Michigan","Texas","Notre Dame" }
 			};
-			int[] salary =
+			int[,] salary =
 			{
-			26400100, 20300100, 1742030, 13100145, 10300000,
-			24500100, 19890200, 18700800, 15000000, 11600400,
-			23400000, 21900300, 19300230, 13400230, 10000000,
-			26200300, 22000000, 16000000, 18000000, 13000000,
-			24000000, 22500249, 20000100, 16000200, 11899999,
-			27800900, 21500249, 20000100, 16000200, 11899999,
-			22900300, 19000590, 18000222, 12999999, 10000000,
-			23000000, 20000000, 19400000, 16200700, 15900000
+			{ 26400100, 20300100, 17420300, 13100145, 10300000 },
+			{ 24500100, 19890200, 18700800, 15000000, 11600400 },
+			{ 23400000, 21900300, 19300230, 13400230, 10000000 },
+			{ 26200300, 22000000, 16000000, 18000000, 13000000 },
+			{ 24000000, 22500249, 20000100, 16000200, 11899999 },
+			{ 27800900, 21500249, 20000100, 16000200, 11899999 },
+			{ 22900300, 19000590, 18000222, 12999999, 10000000 },
+			{ 23000000, 20000000, 19400000, 16200700, 15900000 }
 			};
 			int selection = welcomeOptions();
 			Console.WriteLine(selection.ToString());
